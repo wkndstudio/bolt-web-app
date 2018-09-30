d6623a63f0cfb73732c56b0d6ad68a8056ea9dbc
Hardik Jain <29hdkjain@gmail.com>
2018-09-30 11:55:08 +0530

    Add wicked_pdf

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	#static pages
 	# get "/pages/*page" => "pages#show"
+	resources :invoicing_ledger_items
+	resources :invoices
 	get "/*page" => "pages#show"
 	root 'pages#home'
 end
