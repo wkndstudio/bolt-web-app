1517442254735f5d2af798f95cd1cf1b93472dba
designcrumble <y13uc147@lnmiit.ac.in>
2018-10-03 07:39:56 +0530

    sidebar + header partials + icons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,46 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require_tree .
+
+
+// Change img svg to inline svg -- images need to have .svg class
+$( document ).ready(function() {
+       $('img.svg').each((i, e) => {
+
+        const $img = $(e);
+
+        const imgID = $img.attr('id');
+
+        const imgClass = $img.attr('class');
+
+        const imgURL = $img.attr('src');
+
+        $.get(imgURL, (data) => {
+            // Get the SVG tag, ignore the rest
+            let $svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', `${imgClass}replaced-svg`);
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr(`viewBox 0 0  ${$svg.attr('height')} ${$svg.attr('width')}`);
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+});
